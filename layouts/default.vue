fcfcc90c50cd592ab677f9c837813e27e92320d6
megasharks <tigran.accontech@gmail.com>
2018-10-27 12:24:37 +0400

    smaller version for jquery

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@ export default {
   head () {
     return {
       script: [
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js' },
         { src: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js'}
       ] 
     }
